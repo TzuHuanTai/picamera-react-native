24d361733decd1acab799e7a2f0134feedbc64ce
Tzu Huan Tai <tzu.huan.tai@gmail.com>
2025-04-16 00:11:35 -0700

    fix: emit oncomplete event twice

--- src/rtc/datachannel-receiver.ts
+++ src/rtc/datachannel-receiver.ts
@@ -17,14 +17,12 @@ export class DataChannelReceiver {
     if (this.isFirstPacket) {
       this.fileBuffer = new Uint8Array(Number(arrayBufferToString(packet)));
       this.isFirstPacket = false;
+    } else if (packet.length === 0) {
+      this.reset();
     } else {
       this.fileBuffer.set(packet, this.receivedLength);
       this.receivedLength += packet.length;
       this.onComplete(this.receivedLength, this.fileBuffer);
-    }
-
-    if (packet.length === 0) {
-      this.reset();
     }
   }
 
